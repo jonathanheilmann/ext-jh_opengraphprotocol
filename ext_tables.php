914fef6125aef28eb812bb2cb6397e2207f524ab
Jonathan Heilmann <mail@jonathan-heilmann.de>
2015-06-27 20:39:49 +0200

    [ENHANCEMENT] Add FAL (Related to #6)
    Implemented FAL TCA and functions to render FAL images.
    TODO:
    - migrate old images to FAL
    - remove old image-filed

--- ext_tables.php
+++ ext_tables.php
@@ -36,6 +36,38 @@ $tempColumns = array (
             'maxitems' => 6,
         )
     ),
+	'tx_jhopengraphprotocol_ogfalimages' => array(
+		'exclude' => 1,
+		'label'	=> 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml:pages.tx_jhopengraphprotocol_ogimage',
+        'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+			'tx_jhopengraphprotocol_ogfalimages',
+			array(
+				'appearance' => array(
+					'enableControls' => array(
+						'sort' => TRUE,
+					),
+				),
+				'foreign_types' => array(
+					'0' => array(
+						'showitem' => '
+						--palette--;;opengraphprotocolPalette,
+						--palette--;;filePalette'
+					),
+					\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array(
+						'showitem' => '
+						--palette--;;opengraphprotocolPalette,
+						--palette--;;filePalette'
+					),
+					\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION  => array(
+						'showitem' => '
+						--palette--;;opengraphprotocolPalette,
+						--palette--;;filePalette'
+					),
+				),
+			),
+        	$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+		),
+	),
     'tx_jhopengraphprotocol_ogdescription' => array (
         'exclude' => 1,
         'label' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml:pages.tx_jhopengraphprotocol_ogdescription',
@@ -49,10 +81,10 @@ $tempColumns = array (
 
 // Add columns to TCA of pages and pages_language_overlay
 \TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages',$tempColumns,1);
-\TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages','--div--;LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml:pages.tab_title,tx_jhopengraphprotocol_ogtitle;;;;1-1-1, tx_jhopengraphprotocol_ogtype, tx_jhopengraphprotocol_ogimage, tx_jhopengraphprotocol_ogdescription');
+\TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages','--div--;LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml:pages.tab_title,tx_jhopengraphprotocol_ogtitle;;;;1-1-1, tx_jhopengraphprotocol_ogtype, tx_jhopengraphprotocol_ogimage, tx_jhopengraphprotocol_ogfalimages, tx_jhopengraphprotocol_ogdescription');
 
 \TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay',$tempColumns,1);
-\TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay','--div--;LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml:pages.tab_title,tx_jhopengraphprotocol_ogtitle;;;;1-1-1, tx_jhopengraphprotocol_ogtype, tx_jhopengraphprotocol_ogimage, tx_jhopengraphprotocol_ogdescription');
+\TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay','--div--;LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml:pages.tab_title,tx_jhopengraphprotocol_ogtitle;;;;1-1-1, tx_jhopengraphprotocol_ogtype, tx_jhopengraphprotocol_ogimage, tx_jhopengraphprotocol_ogfalimages, tx_jhopengraphprotocol_ogdescription');
 
 // Add static file
 \TYPO2\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Open Graph protocol');
