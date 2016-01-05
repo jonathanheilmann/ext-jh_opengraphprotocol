a8b4d78a0a06b94c4ded7c84fc5b9274f30a48a5
jonathanheilmann <mail@jonathan-heilmann.de>
2016-01-05 18:22:05 +0100

    [TASK] Implement PSR-2 standard (Resolves #20)

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,9 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-     die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
 }
 
 $TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_jhopengraphprotocol_ogtitle,tx_jhopengraphprotocol_ogtype,tx_jhopengraphprotocol_ogfalimages,tx_jhopengraphprotocol_ogdescription';
 
 // Add update script to install tool
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['jh_opengraphprotocol_fal'] = 'Heilmann\\JhOpengraphprotocol\\Updates\\FalUpdateWizard';
-
-?>
