3c74af9d1ae027e0ecfcf98d08b5a45d8483d189
jonathanheilmann <mail@jonathan-heilmann.de>
2016-08-17 17:33:37 +0200

    [BUGFIX] Not localized og tag on FE (Related to #23)
    Removed deprecated comment

--- Classes/Service/OgRendererService.php
+++ Classes/Service/OgRendererService.php
@@ -112,7 +112,6 @@ class OgRendererService implements \TYPO3\CMS\Core\SingletonInterface
             // Setting 'Default NULL' in database config won't work, too, as it isn't easier to check, if value in
             // $GLOBALS['TSFE']->page['tx_jhopengraphprotocol_ogtype'] is related to table 'pages' or 'pages_language_overlay'.
 
-            // Parameter 3 (uid) is always the pages' uid, as sys_file_reference always references to the pages' uid, even on pages_language_overlay
             $overlayFileObjects = $fileRepository->findByRelation('pages_language_overlay', 'tx_jhopengraphprotocol_ogfalimages', $GLOBALS['TSFE']->page['_PAGES_OVERLAY_UID']);
             if (count($overlayFileObjects) > 0)
                 $fileObjects = $overlayFileObjects;
